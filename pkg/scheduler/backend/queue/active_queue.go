afcd3fd899c52b33d09aa574a3d2450923505b4e
Vlad Shkrabkov <vshkrabkov@google.com>
2025-12-15 13:51:47 +0000

    Refactor PriorityQueue.Update to remove underLock and redundant checks. Removes unnecessary existence checks (activeQ.has, backoffQ.has, unschedulablePods.get) and replaces the activeQ.underLock closure with a self-locking activeQ.add call.

--- pkg/scheduler/backend/queue/active_queue.go
+++ pkg/scheduler/backend/queue/active_queue.go
@@ -42,6 +42,7 @@ type activeQueuer interface {
 	list() []*v1.Pod
 	len() int
 	has(pInfo *framework.QueuedPodInfo) bool
+	add(logger klog.Logger, pInfo *framework.QueuedPodInfo, event string)
 
 	movePodToInFlight(pInfo *framework.QueuedPodInfo) error
 	listInFlightEvents() []interface{}
@@ -372,6 +373,13 @@ func (aq *activeQueue) has(pInfo *framework.QueuedPodInfo) bool {
 	aq.lock.RLock()
 	defer aq.lock.RUnlock()
 	return aq.queue.Has(pInfo)
+}
+
+func (aq *activeQueue) add(logger klog.Logger, pInfo *framework.QueuedPodInfo, event string) {
+	aq.lock.Lock()
+	defer aq.lock.Unlock()
+
+	aq.unlockedQueue.add(logger, pInfo, event)
 }
 
 // listInFlightEvents returns all inFlightEvents.
